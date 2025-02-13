eb11c0761a1a18afbc902f50fe52a1b1d434e0b5
2yuna <y.duck10002@gmail.com>
2025-02-13 16:51:11 +0900

    외부설정 사용 및 검증

---
+++ src/main/java/hello/external/ExternalApplication.java
@@ -0,0 +1,20 @@
+package hello.external;
+
+import hello.external.config.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Import;
+
+//@Import(MyDataSourceEvcConfig.class)
+//@Import(MyDataSourceValueConfig.class)
+@Import(MyDataSourceConfigV3.class)
+@SpringBootApplication(scanBasePackages = "hello.external.datasource")
+@ConfigurationPropertiesScan
+public class ExternalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalApplication.class, args);
+    }
+
+}
